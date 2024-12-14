5724d730e86b74a95be5d580d1b122e92515851a
mishel0312 <mishel0312@gmail.com>
2024-12-14 16:59:05 +0200

    modified MishelYusupov.java

--- MishelYusupov.java
+++ MishelYusupov.java
@@ -1,7 +1,8 @@
 public class MishelYusupov{
 	public static void main(String[] args) {
-		for (int i=1;i<=6; i++) {
+		for (int i=1;i<=3; i++) {
 			System.out.println("Mishel Yusupov");
+			System.out.println("This is an updated version");
 		}
 	}
 }
