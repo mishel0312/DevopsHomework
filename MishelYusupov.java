b381037062558235ee19c4c8a077d872cb2828e2
mishel0312 <mishel0312@gmail.com>
2024-12-14 17:05:22 +0200

    Modified this file again !

--- MishelYusupov.java
+++ MishelYusupov.java
@@ -2,7 +2,7 @@ public class MishelYusupov{
 	public static void main(String[] args) {
 		for (int i=1;i<=3; i++) {
 			System.out.println("Mishel Yusupov");
-			System.out.println("This is an updated version");
+			System.out.println("This is an updated version!");
 		}
 	}
 }
